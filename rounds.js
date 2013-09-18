aed2a3d9f03485e6750c2786179998044c730e31
cooperq <cooper@radicaldesigns.org>
2013-09-17 19:01:34 -0700

    threshold for displaying rounds; contextural timer; formatted timer; basic css

--- rounds.js
+++ rounds.js
@@ -18,22 +18,67 @@ $(function(){
       var now = Math.round(new Date().getTime()/1000);
       return roundEnd - now;
     },
+    isEvent: function(){
+      return !!this.get('isEvent');
+    }
   });
 
   var TimerCollection = Backbone.Collection.extend({
     url: function(){ return '/rounds.json'},
     model: Timer,
+    current: function(){
+      //filter to only show current rounds
+      var threshold = 7200; //threshold in seconds for displaying rounds.  3600 = 1 hour.
+      var rounds = this.filter(function(round){
+        return threshold > Math.abs(round.getTimeRemaining());
+      });
+      return rounds;
+    }
   });
 
   var TimerView = Backbone.View.extend({
     tagName: 'div',
     template: TimerTemplates.timer_container,
+    pad: function(num){
+      if(num > 9){return num}
+      return '0' + num;
+    },
+    formatSeconds: function(secs){
+      var output = '';
+      var hours = parseInt(secs / 3600);
+      var minutes = Math.floor(secs / 60) % 60;
+      var seconds = secs % 60;
+      if(hours > 0){
+        output += hours + ":";
+      }
+      output += this.pad(minutes) + ":" + this.pad(seconds);
+      return output;
+    },
     render: function(){ 
+      var self = this;
+      var timeRemainingStr = function(){
+        if(self.model.getTimeRemaining() < 0){
+          var timer = "";
+          if(self.model.isEvent()){
+            var message = "The event has started";
+          }else{
+            var message = "The round is finished";
+          }
+        } else {
+          var timer = self.formatSeconds(self.model.getTimeRemaining())
+          if(self.model.isEvent()){
+            var message = " remaining until event starts";
+          }else{
+            var message = " left in the round";
+          }
+        }
+        return timer + message;
+      }
       this.$el.html(this.template({
         timer:{
           id: this.model.id,
           name:this.model.get('roundName'),
-          timeRemaining: this.model.getTimeRemaining()
+          timeRemaining: timeRemainingStr()
         }
       }));
       return this;
@@ -43,7 +88,7 @@ $(function(){
   var TimerListView = Backbone.View.extend({
     el: '#content-inner-container',
     render: function(){
-      if(this.collection.length <= 0){ return this };
+      if(this.collection.length <= 0){ this.$el.html('<h1>There are no current events</h1>'); return this };
       this.$el.html('');
       this.addAllTimers();
       return this;
@@ -54,7 +99,7 @@ $(function(){
       this.$el.append(t);
     },
     addAllTimers: function(){
-      this.collection.each(this.addTimer, this);
+      _.each(this.collection.current(),this.addTimer, this);
     }
   });
   
